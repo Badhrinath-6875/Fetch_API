51caebe23da5aff8e7cde7ffa58c25cd8c9d28ba
Badhrinath-6875 <109790751+Badhrinath-6875@users.noreply.github.com>
2022-12-13 21:47:15 +0530

    Update Fetch_api.js

--- Fetch_api.js
+++ Fetch_api.js
@@ -1,23 +1,20 @@
 <!DOCTYPE html>
 <html>
 <body>
-
 <h1>JavaScript Fetch API</h1>
-<h2>The fetch()</h2>
 <p id="demo">Fetch a file to change this text.</p>
 
 <script>
-let file = "https://reqres.in/api/users"
-fetch (file)
-.then(x => x.json())
-.then(y => {
-    var x = "";
-    for(var i=0;i<y.data.length;i++){
-        x +=y.data[i].first_name + "_" + y.data[i].last_name +",";
-    }
-    document.getElementById("demo").innerHTML = x;
-});
+getText('https://reqres.in/api/users');
 
+async function getText(file) {
+  let myFile = await fetch(file);
+  let y = await myFile.json();
+  var x = "";
+  for(var i=0;i<y.data.length;i++)
+    x =x + "  " + y.data[i].first_name;
+  document.getElementById("demo").innerHTML = x;
+}
 </script>
 
 </body>
